b65c591ad41b1dc0ca1717679084131e522ef32c
R. Biswas <rbiswas4@gmail.com>
2017-03-07 20:42:55 -0800

    Added version and location print statements 	modified:   test_skybrightness.py

--- examples/test_skybrightness.py
+++ examples/test_skybrightness.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style('whitegrid')
+import healpy as hp
 
 from lsst.sims.photUtils import Bandpass, BandpassDict
 import lsst.sims.skybrightness as sb
@@ -12,9 +13,15 @@ import os
 from brightness import mCalcs, atmTransName
 from lsst.sims.skybrightness import SkyModel
 
-
+print('Healpy', hp.__version__)
+print('numpy', np.__version__)
+print(getPackageDir('healpy'))
+print(getPackageDir('sims_skybrightness'))
+print(etPackageDir('sims_skybrightness_data'))
+print(etPackageDir('sims_skybrightness_pre'))
+print('numpy', np.__version__)
 tot, hwbpdict = BandpassDict.loadBandpassesFromFiles()
-#print(mCalcs(1.464, 'y', 1.676483, -1.082473, 59580.033829, 1.263038, hwbpdict))
-#print(mCalcs(1.454958, 'y', 1.69412, -1.033972, 59580.034275, 1.258561, hwbpdict))
+print(mCalcs(1.464, 'y', 1.676483, -1.082473, 59580.033829, 1.263038, hwbpdict))
+print(mCalcs(1.454958, 'y', 1.69412, -1.033972, 59580.034275, 1.258561, hwbpdict))
 print(mCalcs(1.008652, 'g', 0.925184, -0.4789, 61044.077855, 1.086662, hwbpdict=hwbpdict))
 print(np.__version__)
