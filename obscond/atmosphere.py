94b2f41c982012a77ca8dc0227001fe0b9d02b63
R. Biswas <rbiswas4@gmail.com>
2017-05-22 15:49:45 -0700

    Added files with codes and test to get airmass dependent bandpass 	modified:   obscond/__init__.py 	new file:   obscond/atmosphere.py 	new file:   tests/test_bandpasses.py

---
+++ obscond/atmosphere.py
@@ -0,0 +1,84 @@
+"""
+A module to describe the atmospheric transmission
+"""
+__all__ = ['AirmassDependentBandpass']
+import os
+import numpy as np
+from lsst.utils import getPackageDir 
+from lsst.sims.photUtils import Bandpass, BandpassDict
+
+def atmTransName(airmass):
+    """
+    return the filename holding the atmospheric transmission with airmass
+    closest to the provided airmass
+
+    Parammeters
+    -----------
+    airmass : `np.float`
+	value of airmass for which we would like to get the atmospheric
+        transmission filename
+    """
+
+    l = np.arange(1.0, 2.51, 0.1)
+    idx = np.abs(l - airmass).argmin()
+    a = np.int(10 * l[idx])
+    baseline = getPackageDir('THROUGHPUTS')
+    fname = os.path.join(baseline, 'atmos', 'atmos_{}_aerosol.dat'.format(a))
+
+    return fname
+
+class AirmassDependentBandpass(object):
+    def __init__(self, HWbandpassDict):
+        """
+	"""
+        self.hwbandpassDict = HWbandpassDict
+
+    @classmethod
+    def fromThroughputs(cls):
+        """
+        instantiate class from 
+	"""
+        totalbpdict, hwbpdict = BandpassDict.loadBandpassesFromFiles()	
+        return cls(HWbandpassDict=hwbpdict)
+
+    @staticmethod
+    def atmTransName(airmass):
+        """
+        return the filename holding the atmospheric transmission with airmass
+        closest to the provided airmass
+    
+        Parammeters
+        -----------
+        airmass : `np.float`
+    	value of airmass for which we would like to get the atmospheric
+            transmission filename
+        """
+    
+        l = np.arange(1.0, 2.51, 0.1)
+        idx = np.abs(l - airmass).argmin()
+        a = np.int(10 * l[idx])
+        baseline = getPackageDir('THROUGHPUTS')
+        fname = os.path.join(baseline, 'atmos', 'atmos_{}_aerosol.dat'.format(a))
+    
+        return fname
+
+    def bandpassForAirmass(self, bandname, airmass=1.2):
+        """
+        return the `lsst.sims.photUtils.Bandpass` object corresponding to the
+	bandname and airmass
+
+        Parameters
+        ----------
+        bandname : mandatory, string
+            name of the band, which must match the dictionary keys on the
+            `lsst.sims.photUtils.BandpassDict`
+        airmass : float, defaults to 1.2
+            value of airmass for which we want to obtain the bandpass
+        """
+        fname = self.atmTransName(airmass)
+        atmTrans = np.loadtxt(fname)
+        wave, trans = self.hwbandpassDict[bandname].multiplyThroughputs(atmTrans[:, 0],
+								  atmTrans[:, 1])
+
+        return Bandpass(wavelen=wave, sb=trans)
+
