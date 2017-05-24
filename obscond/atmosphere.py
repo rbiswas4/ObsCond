49d85311826629fff445ae6c033165cdf5ebef2e
R. Biswas <rbiswas4@gmail.com>
2017-05-24 11:03:05 -0700

    added tests for atmospheric inputs

--- obscond/atmosphere.py
+++ obscond/atmosphere.py
@@ -2,44 +2,39 @@
 A module to describe the atmospheric transmission
 """
 __all__ = ['AirmassDependentBandpass']
+
 import os
 import numpy as np
 from lsst.utils import getPackageDir 
 from lsst.sims.photUtils import Bandpass, BandpassDict
 
-def atmTransName(airmass):
-    """
-    return the filename holding the atmospheric transmission with airmass
-    closest to the provided airmass
-
-    Parammeters
-    -----------
-    airmass : `np.float`
-	value of airmass for which we would like to get the atmospheric
-        transmission filename
-    """
-
-    l = np.arange(1.0, 2.51, 0.1)
-    idx = np.abs(l - airmass).argmin()
-    a = np.int(10 * l[idx])
-    baseline = getPackageDir('THROUGHPUTS')
-    fname = os.path.join(baseline, 'atmos', 'atmos_{}_aerosol.dat'.format(a))
-
-    return fname
 
 class AirmassDependentBandpass(object):
-    def __init__(self, HWbandpassDict):
+    """
+    Class to describe airmass dependent bandpasses. The method that provides
+    a `lsst.sims.photUtils.bandpass` object for the appropriate bandpassname 
+    and airmass is the method `bandpassForAirmass`
+    """
+    def __init__(self, hwBandpassDict):
         """
+        Parameters
+        ----------
+        hwBandpassDict: `lsst.sims.photUtils.BandpassDict` object 
+            Hardware BandpassDict for the system bandpass (no atmosphere) only.
+            This is a dictionary with keys given by each of the bandpass filter
+            names 'ugrizy' with values equal to the corresponding
+            `lsst.sims.Bandpass` object`
 	"""
-        self.hwbandpassDict = HWbandpassDict
+        self.hwbandpassDict = hwBandpassDict
 
     @classmethod
     def fromThroughputs(cls):
         """
-        instantiate class from 
+        instantiate class from the LSST throughputs in the throughputs
+        directory
 	"""
         totalbpdict, hwbpdict = BandpassDict.loadBandpassesFromFiles()	
-        return cls(HWbandpassDict=hwbpdict)
+        return cls(hwBandpassDict=hwbpdict)
 
     @staticmethod
     def atmTransName(airmass):
