5b2b8a5a635ff7590f8152d376354ca7c4dfcb37
R. Biswas <rbiswas4@gmail.com>
2019-04-05 14:34:42 +0200

    Aded notebook for example 	new file:   ../examples/dc2_cadence.ipynb 	modified:   observingPotential.py 	modified:   version.py

--- obscond/observingPotential.py
+++ obscond/observingPotential.py
@@ -5,8 +5,9 @@ import time
 import numpy as np
 import pandas as pd
 
+
 import lsst.sims.skybrightness as sb
-from lsst.sims.utils import Site
+from lsst.sims.utils import (Site, approx_RaDec2AltAz)
 import ephem
 from obscond import SkyCalculations as sm
 
@@ -15,11 +16,24 @@ from lsst.sims.utils import angularSeparation
 from scipy.interpolate import interp1d
 
 class ObservationPotential(object):
+    """
+    Class to define the potential for observations
+
+
+    Parameters
+    ----------
+    fieldRA : float, in radians
+        RA of the field
+    fieldDec : float, in radians
+        Dec of the field
+    observatory : string, defaults to `LSST`
+        string specifying observatory to `ephem`
+    """
     def __init__(self, fieldRA, fieldDec,
                  observatory='LSST'):
-        self.ra = fieldRA
-        self.dec = fieldDec
-        self.site = Site('LSST')
+        self.ra = np.degrees(fieldRA)
+        self.dec = np.degrees(fieldDec)
+        self.site = Site(observatory)
         self.sun = ephem.Sun()
         self.moon = ephem.Moon()
         self.obs = ephem.Observer()
@@ -36,7 +50,14 @@ class ObservationPotential(object):
         
         Parameters
         ----------
-        
+        mjd : `np.float`, unit day
+            time of observation
+
+        Returns
+        -------
+        moon ra: radians
+        moon dec: radians
+        moon alt: radians
         """
         self.obs.date = mjd - self.doff
         self.moon.compute(self.obs)
@@ -48,57 +69,97 @@ class ObservationPotential(object):
         
         Parameters
         ----------
-        tt : float
+        tt : `np.float`, unit day
             time at which altitude is desired in mjd
             
-        Return:
-        in radians
+        Returns
+        -------
+        sun alt: radians
         """
         self.obs.date = tt - self.doff
         self.sun.compute(self.obs)
+
         return self.sun.alt
     
     def sunAlt(self, mjd):
+        """return the sun Altitude at an array of times in degrees
+
+        Parameters
+        ----------
+        mjd : array-like, MJD
+            sequence of times of observation in MJD
+        """
         sunAlt = np.degrees(list(self.sunAlt_singleTime(tt) for tt in mjd))
         return sunAlt
     
     def moonCoords(self, mjd):
+        """return the moon coordinates (ra, dec, Alt) at an array of times in
+        degrees
+
+
+        Parameters
+        ----------
+        mjd : sequence of floats
+            Times in Modified Julian Date
+
+        Returns
+        -------
+        moon ra: degrees
+        moon dec: degrees
+        moon alt: degrees
+        """
         moonRA, moonDec, moonAlt = list(zip(*(self.moonCoords_singleTime(tt) for tt in mjd)))
         return np.degrees(moonRA), np.degrees(moonDec), np.degrees(moonAlt)
 
     
     def field_coords(self, fieldRA, fieldDec, mjd):
-        """
-        Return the field Coordinates in degrees.
+        """Return the field Coordinates in degrees.
         
         Parameters
         ----------
-        fieldRA : float, or array, radians
+        fieldRA : float, or array, degrees
             RA of the field
-        fiedlDec : float or array, radians
+        fiedlDec : float or array, degrees
             Dec of the field
         mjd : float or array
-        
+        Returns
+        -------
+        alt : degrees
+        az : degrees 
         """
         ra = np.ravel(fieldRA)
         dec = np.ravel(fieldDec)
         mjd = np.ravel(mjd)
-        alt, az = sb.stupidFast_RaDec2AltAz(ra, dec,
-                                            self.site.latitude_rad,
-                                            self.site.longitude_rad,
-                                            mjd=mjd)
-        return np.degrees(alt), np.degrees(az)
+
+        # note that all angle arguments are in degrees
+        alt, az = approx_RaDec2AltAz(ra=ra, dec=dec,
+                                     lat=self.site.latitude,
+                                     lon=self.site.longitude,
+                                     mjd=mjd,
+                                     lmst=None)
+        return alt, az
     
     def potential_obscond(self, t, fieldRA, fieldDec,
                           nightOffset=59579.6):
         """
+        Calculate the observing Conditions at a sequence of mjd values
+         `t` for a location with ra and dec `fieldRA` and `fieldDec` in
+         `degrees`.
         Parameters
         ----------
         t : times at which observations are being made
+
+        fieldRA : RA, degree
+
+        fieldDec : Dec, degree
+
+        nightOffset : mjd value, defaults to 59579.6
+            mjd value for night = 0 of the survey.
         """
         alt, az = self.field_coords(fieldRA, fieldDec, mjd=t)
         moonra, moondec, moonalt = self.moonCoords(mjd=t)
         sunAlt = self.sunAlt(mjd=t)
+
         df = pd.DataFrame(dict(mjd=t,
                        alt=alt,
                        az=az, 
@@ -107,37 +168,55 @@ class ObservationPotential(object):
                        moonDec=moondec,
                        moonAlt=moonalt,
                        night=np.floor(t-59579.6).astype(np.int)))
+
         df['moonDist'] = angularSeparation(moonra, moondec,
                                            np.degrees(self.ra),
                                            np.degrees(self.dec))
-        #df.sunAlt = df.sunAlt.apply(np.degrees)
         
         return df
     
     def available_times(self, potential_times, constraints):
-        
+        """returns available times 
+        """
         self._available_times = potential_times.query(constraints)
         return self._available_times
     
     @staticmethod
     def dc2_sequence(start_time, year_block, delta=1):
+        """
+
+        Parameters
+        ----------
+        start_time :
+
+        year_block :
+
+        delta : , defaults to 1.
+        """
         sec = 1.0 / 24.0/60./60.
+
         if year_block == 1:
             fraction = 0.75
+
         if year_block == 2:
             fraction = 0.5
             delta = 0.
+
         standard_sequence = np.array(list((20, 10, 20, 26, 20)))
-        time  = start_time
+        time = start_time
         l = []
         visitlist = []
         seq = standard_sequence 
         extravisits = np.array([0, 1, 0, 1, 0]) * delta
         #print(seq)
         bandlist = []
+
+        # Iterate through the standard list of band, standard visit sequences
+        # For on and off years
         for band, visits, morevisits in zip(list('rgizy'), seq, extravisits):
             numVisits = np.floor(visits * fraction) + morevisits
-            times = np.linspace(time, start_time + (numVisits -1) * 38 *sec, numVisits)
+            times = np.linspace(time, time + (numVisits - 1) * 38 * sec,
+                                numVisits)
             l.append(times)
             visitlist.append(numVisits)
             time = times[-1] + 150.0 * sec
@@ -151,18 +230,26 @@ class ObservationPotential(object):
         
         vs = []
         for st in start_times.values:
-            df, vl = ObservationPotential.dc2_sequence(st, year_block, delta=1)
+            df, vl = ObservationPotential.dc2_sequence(st, year_block, delta)
             vs.append(df)
         df = pd.concat(vs)
         df['night'] = np.floor(df.expMJD - 59579.6)
+
         if pointings is not None:
-            rawSeeing = interp1d(pointings.expMJD.values, 
+            rawSeeing = interp1d(pointings.expMJD.values,
                                  pointings.rawSeeing.values,
                                  kind='nearest')
-            df['rawSeeing'] = interp1d(df.expMJD.values)
-            
+
+            df['rawSeeing'] = rawSeeing(df.expMJD.values)
+            alt, az = approx_RaDec2AltAz(ra=ra, dec=dec,
+                                         lat=self.site.latitude,
+                                         lon=self.site.longitude,
+                                         mjd=df.expMJD.values,
+                                         lmst=None)
+            df['alt'] = alt
+            df['az'] = az
+   
         return df
-        
     
     @staticmethod
     def nightStats(availabletimes):
